e591e4585f20a250ecf0e80671fcf918c9614bd2
gumber99 <98549991+gumber99@users.noreply.github.com>
2022-01-27 23:44:38 +0530

    Add files via upload

---
+++ fsds python1.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+# coding: utf-8
+1. In the below elements which of them are values or an expression? eg:- values can be integer or string and expressions will be mathematical operators.
+* 
+'hello'
+-87.8
+- 
+/ 
++	
+6 
+
+exppression 
+value 
+value 
+expression 
+expression
+expression
+value
+
+2. What is the difference between string and variable?
+
+   string: string can be written in quotes such: "parth"3. Describe three different data types?
+   
+  1)list:   it is used to store multiple data item in a single variable.
+  2)intger: it can store only numerical value.
+  3)float:   it can store onl;y floating values such as 2.3, 28.5.4. What is an expression made up of? What do all expressions do?
+
+  expression is made up of values, containers, mathematical operators eg: a=5+2
+  expression are used to evaluate the values or show the result on the screen.
+ 
+  5. This assignment statements, like spam = 10. What is the difference between an expression and a statement?
+
+  expression is made up os values, containers,matheatical operators and statement is like a commmand thet a python operator executes. 6. After running the following code, what does the variable bacon contain?
+bacon = 22
+bacon + 1  
+  
+  it gives 23 as an excution code.
+# In[13]:
+
+
+bacon =22
+
+
+# In[14]:
+
+
+bacon+1
+
+7. What should the values of the following two terms be?
+'spam' + 'spamspam'
+'spam' * 3 
+
+ it would be 'spamspamspam'
+ it would be 'spanspanspan'
+# In[15]:
+
+
+'spam' + 'spamspam'
+
+
+# In[16]:
+
+
+'span'*3
+
+8. Why is eggs a valid variable name while 100 is invalid?
+
+  Because we can't start giving variable an integer name. if we, we should begin with, a string-like alphabet name then integer. e100 or eggs100 is valid.
+  9. What three functions can be used to get the integer, floating-point number, or string version of a value?
+
+  str(), int(), float()10. Why does this expression cause an error? How can you fix it?
+'I have eaten ' + 99 + ' burritos.'
+  
+  Because 99 is an integer it cannot be concatenated with strings, if we have to concatenate it we need to do typecasting. 
